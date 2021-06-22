b11f3d53a8ce90d928a7622764dc77fe3384ed70
bwalsh11 <walsh.swc@gmail.com>
2021-06-22 12:54:29 -0700

    updating

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Thumb services',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A business connecting dev shops with potential clients',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
