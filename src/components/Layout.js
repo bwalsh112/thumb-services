38042a010f3dafbcbc63004f052765ff4fd0399a
bwalsh11 <walsh.swc@gmail.com>
2021-07-18 13:03:32 -0700

    trying to adjust og

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -45,7 +45,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
+          content={`https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80`}
         />
       </Helmet>
       {/* <Navbar /> */}
